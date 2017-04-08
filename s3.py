ac38c5a06ef7a0854fd18930fa41e29b6af2714f
Kevin Chung <kchung@nyu.edu>
2017-04-08 02:33:53 -0400

    Boto3 S3 Plugin

--- s3.py
+++ s3.py
@@ -1,13 +1,12 @@
 from flask import request, redirect, abort, jsonify, url_for
-from CTFd.utils import is_admin, ctftime, user_can_view_challenges, view_after_ctf, admins_only
 from CTFd.models import db, Solves, Challenges, WrongKeys, Keys, Tags, Files
 
+from CTFd import utils
 import os
-import boto
+import boto3
 import hashlib
 import string
 from werkzeug.utils import secure_filename
-from boto.s3.key import Key
 
 
 def clean_filename(c):
@@ -16,147 +15,79 @@ def clean_filename(c):
 
 
 def get_s3_conn(app):
-    if app.config.get('ACCESS_KEY_ID') and app.config.get('SECRET_ACCESS_KEY'):
-        return boto.connect_s3(app.config.get('ACCESS_KEY_ID'), app.config.get('SECRET_ACCESS_KEY'))
+    access_key_id = utils.get_config('ACCESS_KEY_ID')
+    secret_access_key = utils.get_config('SECRET_ACCESS_KEY')
+    if access_key_id and secret_access_key:
+        client = boto3.client(
+            's3',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key
+        )
+        bucket = utils.get_config('BUCKET')
+        return client, bucket
     else:
-        return boto.connect_s3()
+        client = boto3.client('s3')
+        bucket = utils.get_config('BUCKET')
+        return client, bucket
 
 
 def load(app):
+    def upload_file(file, chalid):
+        s3, bucket = get_s3_conn(app)
+
+        filename = filter(clean_filename, secure_filename(file.filename).replace(' ', '_'))
+        if len(filename) <= 0:
+            return False
+
+        md5hash = hashlib.md5(os.urandom(64)).hexdigest()
+
+        key = md5hash + '/' + filename
+        print file
+        print bucket, key
+        s3.upload_fileobj(file, bucket, key)
+
+        db_f = Files(chalid, key)
+        db.session.add(db_f)
+        db.session.commit()
+        return True
+
+    def delete_file(filename):
+        s3, bucket = get_s3_conn(app)
+        f = Files.query.filter_by(id=filename).first_or_404()
+        key = f.location
+        s3.delete_object(Bucket=bucket, Key=key)
+        db.session.delete(f)
+        db.session.commit()
+        return True
+
     def file_handler(path):
         f = Files.query.filter_by(location=path).first_or_404()
         chal = Challenges.query.filter_by(id=f.chal).first()
 
-        if is_admin():
-            s3 = get_s3_conn(app)
-            bucket_name = app.config.get('BUCKET')
-            bucket = s3.get_bucket(bucket_name)
-            k = Key(bucket)
-            k.key = f.location
-            url = k.generate_url(expires_in=600, query_auth=True)
+        s3, bucket = get_s3_conn(app)
+        if utils.is_admin():
+            key = f.location
+            url = s3.generate_presigned_url('get_object', Params = {
+                'Bucket': bucket,
+                'Key': key, })
             return redirect(url)
 
-        if user_can_view_challenges():
-            if not ctftime():
-                if not view_after_ctf():
+        if utils.user_can_view_challenges():
+            if not utils.ctftime():
+                if not utils.view_after_ctf():
                     abort(403)
 
             if chal.hidden:
                 abort(403)
 
-            s3 = get_s3_conn(app)
-            bucket_name = app.config.get('BUCKET')
-            bucket = s3.get_bucket(bucket_name)
-            k = Key(bucket)
-            k.key = f.location
-            url = k.generate_url(expires_in=600, query_auth=True)
+            key = f.location
+            url = s3.generate_presigned_url('get_object', Params = {
+                'Bucket': bucket,
+                'Key': key, })
             return redirect(url)
         else:
             return redirect(url_for('auth.login'))
 
-    @admins_only
-    def admin_files(chalid):
-        if request.method == 'GET':
-            files = Files.query.filter_by(chal=chalid).all()
-            json_data = {'files': []}
-            for x in files:
-                json_data['files'].append({'id': x.id, 'file': x.location})
-            return jsonify(json_data)
-        if request.method == 'POST':
-            s3 = get_s3_conn(app)
-            bucket_name = app.config.get('BUCKET')
-            bucket = s3.get_bucket(bucket_name)
-            k = Key(bucket)
-            if request.form['method'] == "delete":
-                f = Files.query.filter_by(id=request.form['file']).first_or_404()
-                k.key = f.location
-                bucket.delete_key(k)
-                db.session.delete(f)
-                db.session.commit()
-                db.session.close()
-                return "1"
-            elif request.form['method'] == "upload":
-                files = request.files.getlist('files[]')
-
-                for f in files:
-                    filename = filter(clean_filename, secure_filename(f.filename).replace(' ', '_'))
-
-                    if len(filename) <= 0:
-                        continue
-
-                    md5hash = hashlib.md5(os.urandom(64)).hexdigest()
-
-                    file_contents = f.read()
-                    k.key = md5hash + '/' + filename
-                    k.set_contents_from_string(file_contents)
-
-                    db_f = Files(chalid, md5hash + '/' + filename)
-                    db.session.add(db_f)
-
-                db.session.commit()
-                db.session.close()
-                return redirect(url_for('admin_challenges.admin_chals'))
-
-    @admins_only
-    def admin_create_chal():
-        files = request.files.getlist('files[]')
-
-        ## TODO: Expand to support multiple flags
-        flags = [{'flag': request.form['key'], 'type': int(request.form['key_type[0]'])}]
-        chal = Challenges(request.form['name'], request.form['desc'], request.form['value'], request.form['category'], flags)
-        if 'hidden' in request.form:
-            chal.hidden = True
-        else:
-            chal.hidden = False
-        db.session.add(chal)
-        db.session.commit()
-
-        s3 = get_s3_conn(app)
-        bucket_name = app.config.get('BUCKET')
-        bucket = s3.get_bucket(bucket_name)
-        k = Key(bucket)
-
-        for f in files:
-            filename = filter(clean_filename, secure_filename(f.filename).replace(' ', '_'))
-
-            if len(filename) <= 0:
-                continue
-
-            md5hash = hashlib.md5(os.urandom(64)).hexdigest()
-
-            file_contents = f.read()
-            k.key = md5hash + '/' + filename
-            k.set_contents_from_string(file_contents)
-
-            db_f = Files(chal.id, md5hash + '/' + filename)
-            db.session.add(db_f)
-        db.session.commit()
-        db.session.close()
-        return redirect(url_for('admin_challenges.admin_chals'))
-
-    @admins_only
-    def admin_delete_chal():
-        challenge = Challenges.query.filter_by(id=request.form['id']).first()
-        if challenge:
-            WrongKeys.query.filter_by(chalid=challenge.id).delete()
-            Solves.query.filter_by(chalid=challenge.id).delete()
-            Keys.query.filter_by(chal=challenge.id).delete()
-            files = Files.query.filter_by(chal=challenge.id).all()
-            Files.query.filter_by(chal=challenge.id).delete()
-            s3 = get_s3_conn(app)
-            bucket_name = app.config.get('BUCKET')
-            bucket = s3.get_bucket(bucket_name)
-            k = Key(bucket)
-            for file in files:
-                k.key = file.location
-                bucket.delete_key(k)
-            Tags.query.filter_by(chal=challenge.id).delete()
-            Challenges.query.filter_by(id=challenge.id).delete()
-            db.session.commit()
-            db.session.close()
-        return '1'
-
+    utils.upload_file = upload_file
+    utils.delete_file = delete_file
     app.view_functions['views.file_handler'] = file_handler
-    app.view_functions['admin_challenges.admin_create_chal'] = admin_create_chal
-    app.view_functions['admin_challenges.admin_files'] = admin_files
-    app.view_functions['admin_challenges.admin_delete_chal'] = admin_delete_chal
